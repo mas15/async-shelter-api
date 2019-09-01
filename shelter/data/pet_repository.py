901c5040480da67973495d0c8bbb9c3b1a83f2b0
Mateusz Stankiewicz <stanek.94@o2.pl>
2019-09-01 22:20:23 +0200

    Add pet repository, entrypoint

---
+++ shelter/data/pet_repository.py
@@ -0,0 +1,72 @@
+from contextlib import contextmanager, asynccontextmanager
+from typing import Dict
+
+from shelter.data import db
+from shelter.entities.pet_data_storage import PetDataStorage
+
+
+class PetModel(db.Model):
+    __tablename__ = 'pets'
+
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.Unicode(255))
+    type = db.Column(db.Unicode(255))
+
+    available = db.Column(db.Boolean())
+    addedAt = db.Column(db.Unicode(255)) # db.DateTime()
+    adoptedAt = db.Column(db.Unicode(255)) # db.DateTime()
+    description = db.Column(db.Unicode(255))
+    shelterID = db.Column(db.Unicode(255))
+    # shelterID = db.Column(None, db.ForeignKey('shelters.id'))
+
+    def asdict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'type': self.type,
+            'available': self.available,
+            'addedAt': self.addedAt,
+            'adoptedAt': self.adoptedAt,
+            'description': self.description,
+            'shelterID': self.shelterID
+        }
+
+
+class PostgresPetsRepository(PetDataStorage):
+    def __init__(self, gino_engine):
+        self._engine = gino_engine
+
+    @asynccontextmanager
+    async def transaction(self):
+        async with self._engine.acquire() as connection:
+            async with connection.transaction():
+                yield
+
+    async def add(self, pet_data: Dict):
+        async with self.transaction():
+            p = await PetModel.create(**pet_data)
+            return p
+
+    async def get_by_id(self, pet_id: int):
+        async with self.transaction():
+            pet = await PetModel.get(pet_id)
+            return pet
+
+    async def get_by_shelter(self, shelter_id):
+        pets = await PetModel.query.where(PetModel.shelterID == shelter_id).gino.all()
+        return pets
+
+    async def all(self, pet_type=None, shelter_id=None):
+        async with self.transaction():
+            pets = await PetModel.query.gino.all()
+            return pets
+
+    async def update(self, pet_id, data):
+        async with self.transaction():
+            pet = await PetModel.get(pet_id)
+            pet.update(**data).apply()
+            return pet
+
+    async def delete(self, pet_id):
+        async with self.transaction():
+            await PetModel.delete.where(PetModel.id == pet_id)
---
+++ shelter/entrypoint.py
@@ -0,0 +1,36 @@
+import logging
+
+from aiohttp import web
+
+from shelter.data.pet_repository import PostgresPetsRepository
+from shelter.service import APIService
+from shelter.data import db
+
+
+async def make_app():
+    logging.info('STARTING....')
+    app = web.Application()
+
+    gino_engine = await db.set_bind('postgresql://postgres@postgres:5432')
+    await db.gino.create_all()
+    pets_repo = PostgresPetsRepository(gino_engine)
+    logging.info('DB STARTED.....')
+    handler = APIService(pets_repo)
+    app.add_routes([
+        web.get('/pets', handler.pet_list),
+        web.post('/pets', handler.pet_create),
+        web.patch('/pets', handler.pet_update),
+        web.delete('/pets', handler.pet_delete),
+        web.get('/pets/{pet_id}', handler.pet_retrieve),
+        #
+        # web.get('/shelters', handler.shelter_list),
+        # web.post('/shelters', handler.shelter_create),
+        # web.get('/shelters/{shelter_id}', handler.shelter_retrieve),
+        # web.get('/shelters/{shelter_id}/pets', handler.shelter_retrieve_pets)
+    ])
+    return app
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
+    web.run_app(make_app())
